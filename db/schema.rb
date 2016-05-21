348abee8546192b165da0cd5281972cab6ad2071
catheraaine <catheraaine@gmail.com>
2016-05-21 18:07:58 -0400

    Feat: Ketchup

--- db/schema.rb
+++ db/schema.rb
@@ -16,11 +16,6 @@ ActiveRecord::Schema.define(version: 20160521214751) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "locates", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "locations", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
